91448c1c8a94f25ca6c8a8e03e28d67b9a69b622
Hari <hnnhariharan12@gmail.com>
2023-04-24 13:03:04 -0400

    added vit model2

--- models/VisionTransformer.py
+++ models/VisionTransformer.py
@@ -2,11 +2,15 @@ import torchvision
 import torchvision.models.vit_b_16 as vit_b_16
 
 ##Test
-# from pytorch_pretrained_vit import ViT
-# model = ViT('B_16_imagenet1k', pretrained=True)
+from pytorch_pretrained_vit import ViT
 
 
 # vit_b_16
 def vit_model1():
     model = vit_b_16(weights=torchvision.models.ViT_B_16_Weights.IMAGENET1K_V1)
+    return model
+
+
+def vit_model2():
+    model = ViT("B_16_imagenet1k", pretrained=True)
     return model
